3787334e74101305326cd204a79908fb19c71814
Dhruvit07 <salatd0852@gmail.com>
2022-06-10 19:23:13 +0530

    Added Favicon and Title

--- templates/header.php
+++ templates/header.php
@@ -1,10 +1,13 @@
 <?php
+require 'C:/xampp/htdocs/project-1/includes/path-config.inc.php';
 // define( 'ROOT_DIR', dirname(__FILE__) );
-$url = "/project-1";
 // session_start();
 if (!defined("MYSITE")) {
 	header("location: ../public/404");
 }
+$_url = $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+require 'C:/xampp/htdocs/project-1/src/process/category.process.php';
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -15,27 +18,28 @@ if (!defined("MYSITE")) {
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
-	<title></title>
+	<title>Mango Emporium</title>
+	<link rel="icon" type="image/x-icon" href="<?php echo $htmlPath; ?>/resources/img/favicon.png">
 
 	<!-- Google font -->
 	<link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700" rel="stylesheet">
 
 	<!-- Bootstrap -->
-	<!-- <link type="text/css" rel="stylesheet" href="<?php echo $url;?>/resources/css/bootstrap.css" /> -->
-	<link type="text/css" rel="stylesheet" href="<?php echo $url;?>/resources/css/bootstrap.min.css" />
+	<!-- <link type="text/css" rel="stylesheet" href="<?php echo $htmlPath; ?>/resources/css/bootstrap.css" /> -->
+	<link type="text/css" rel="stylesheet" href="<?php echo $htmlPath; ?>/resources/css/bootstrap.min.css" />
 
 	<!-- Slick -->
-	<link type="text/css" rel="stylesheet" href="<?php echo $url;?>/resources/css/slick.css" />
-	<link type="text/css" rel="stylesheet" href="<?php echo $url;?>/resources/css/slick-theme.css" />
+	<link type="text/css" rel="stylesheet" href="<?php echo $htmlPath; ?>/resources/css/slick.css" />
+	<link type="text/css" rel="stylesheet" href="<?php echo $htmlPath; ?>/resources/css/slick-theme.css" />
 
 	<!-- nouislider -->
-	<link type="text/css" rel="stylesheet" href="<?php echo $url;?>/resources/css/nouislider.min.css" />
+	<link type="text/css" rel="stylesheet" href="<?php echo $htmlPath; ?>/resources/css/nouislider.min.css" />
 
 	<!-- Font Awesome Icon -->
-	<link rel="stylesheet" href="<?php echo $url;?>/resources/css/font-awesome.min.css">
+	<link rel="stylesheet" href="<?php echo $htmlPath; ?>/resources/css/font-awesome.min.css">
 
 	<!-- Custom stlylesheet -->
-	<link type="text/css" rel="stylesheet" href="<?php echo $url;?>/resources/css/style.css" />
+	<link type="text/css" rel="stylesheet" href="<?php echo $htmlPath; ?>/resources/css/style.css" />
 
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -54,17 +58,17 @@ if (!defined("MYSITE")) {
 		<div id="top-header">
 			<div class="container">
 				<ul class="header-links pull-left">
-					<li><a href="#"><i class="fa fa-phone"></i> +021-95-51-84</a></li>
-					<li><a href="#"><i class="fa fa-envelope-o"></i> email@email.com</a></li>
-					<li><a href="#"><i class="fa fa-map-marker"></i> 1734 Stonecoal Road</a></li>
+					<li><a href="#"><i class="fa fa-phone"></i> <?php echo $contact['contact']; ?></a></li>
+					<li><a href="#"><i class="fa fa-envelope-o"></i> <?php echo $contact['email']; ?></a></li>
+					<li><a href="#"><i class="fa fa-map-marker"></i> <?php echo $contact['address']; ?></a></li>
 				</ul>
 				<ul class="header-links pull-right">
 					<!-- <li><a href="#"><i class="fa fa-dollar"></i> USD</a></li> -->
 					<?php if (isset($_SESSION['loggedin'])) { ?>
-						<li><a href="<?php echo $url;?>/public/user/?profile"><i class="fa fa-user-o"></i> My Account</a></li>
+						<li><a href="<?php echo $htmlPath; ?>/public/user/?profile"><i class="fa fa-user-o"></i> My Account</a></li>
 					<?php } else { ?>
-						<li><a href="<?php echo $url;?>/public/auth/?register"><i class="fa fa-user-o"></i> Join here</a></li>
-						<li><a href="<?php echo $url;?>/public/auth/"><i class="fa fa-user-o"></i> Sign in</a></li>
+						<li><a href="<?php echo $htmlPath; ?>/public/auth/?register"><i class="fa fa-user-o"></i> Join here</a></li>
+						<li><a href="<?php echo $htmlPath; ?>/public/auth/"><i class="fa fa-user-o"></i> Sign in</a></li>
 					<?php } ?>
 				</ul>
 			</div>
@@ -80,8 +84,8 @@ if (!defined("MYSITE")) {
 					<!-- LOGO -->
 					<div class="col-md-3">
 						<div class="header-logo">
-							<a href="#" class="logo">
-								<img src="<?php echo $url;?>/resources/img/logo.png" alt="">
+							<a href="#" class="logo img-responsive">
+								<img style="height:70px;width:220px;object-fit:cover;" src="<?php echo $htmlPath; ?>/resources/img/logo.svg" alt="">
 							</a>
 						</div>
 					</div>
@@ -93,11 +97,10 @@ if (!defined("MYSITE")) {
 							<form>
 								<select class="input-select">
 									<option value="0">All Categories</option>
-									<option value="1">Category 01</option>
-									<option value="1">Category 02</option>
+									<?php echo $searchBarCategoryOptionHTML; ?>
 								</select>
 								<input class="input" placeholder="Search here">
-								<button class="search-btn">Search</button>
+								<button class="search-btn" style="text-align:center;">Search</button>
 							</form>
 						</div>
 					</div>
@@ -144,13 +147,23 @@ if (!defined("MYSITE")) {
 			<div id="responsive-nav">
 				<!-- NAV -->
 				<ul class="main-nav nav navbar-nav">
-					<li class="active"><a href="#">Home</a></li>
-					<li><a href="#">Hot Deals</a></li>
-					<li><a href="#">Categories</a></li>
-					<li><a href="#">Laptops</a></li>
-					<li><a href="#">Smartphones</a></li>
-					<li><a href="#">Cameras</a></li>
-					<li><a href="#">Accessories</a></li>
+					<?php
+					if (strpos($_url, 'store') !== false) {
+						echo '<li ><a href="' . $htmlPath . '/public">Home</a></li>
+								<li class="active"><a href="' . $htmlPath . '/public/store/">Our Products</a></li>
+								<li><a href="' . $htmlPath . '/public/category">Categories</a></li>';
+					} else if (strpos($_url, 'category') !== false) {
+						echo '<li ><a href="' . $htmlPath . '/public">Home</a></li>
+							<li><a href="' . $htmlPath . '/public/store/">Our Products</a></li>
+							<li class="active"><a href="' . $htmlPath . '/public/category">Categories</a></li>';
+					} else if (strpos($_url, 'public') !== false) {
+						echo '<li class="active"><a href="' . $htmlPath . '/public">Home</a></li>
+							<li><a href="' . $htmlPath . '/public/store/">Our Products</a></li>
+							<li><a href="' . $htmlPath . '/public/category">Categories</a></li>';
+					}
+					?>
+
+
 				</ul>
 				<!-- /NAV -->
 			</div>
